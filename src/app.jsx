d057aca634e3d6e0365510cbf74ecc4e0fb74d8b
brijesh-pant <pantbrijesh717@live.com>
2018-08-10 20:25:02 +0530

    scss changed to sass

--- src/app.jsx
+++ src/app.jsx
@@ -9,6 +9,7 @@ import 'normalize.css';
 
 import 'styles/base/_main.sass'; // Global styles
 import 'styles/base/_common.sass'; // Global styles
+import 'styles/base/index.sass';
 import styles from './app.sass'; // Css-module styles
 
 export default class App extends React.Component {
